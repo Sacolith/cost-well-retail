c194fcec2f314517a7bfb0872243ec427a9df90c
Sacolith <sacqsltd@gmail.com>
2022-07-02 13:06:54 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material';
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return <Layout>List Products</Layout>;
+}
